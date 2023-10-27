771725a0c7642691efaf8b5026d98fbbaf3b6eb8
Sertac Ozercan <sozercan@gmail.com>
2023-10-27 21:39:29 +0000

    update
    Signed-off-by: Sertac Ozercan <sozercan@gmail.com>

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
-# Gatekeeper Demo for Kubecon NA 2023
+# Gatekeeper Demo
 
-This demo shows how to use Gatekeeper with both CEL and Rego-based policies, with constraint violations sent as events to a subscriber using Dapr.
+This demo shows how to use [Gatekeeper](https://open-policy-agent.github.io/gatekeeper/) with both [Common Expression Language (CEL)](https://github.com/google/cel-spec) and [Rego](https://www.openpolicyagent.org/docs/latest/policy-language/) based policies side-by-side, with constraint violations published as messages to a subscriber using [Dapr](https://dapr.io/).
 
-## Prerequisites
+> [!WARNING]
+> Gatekeeper CEL-based policies are a prototype-stage feature and is subject to change.
+
+## Demo
+[![asciicast](https://asciinema.org/a/617530.svg)](https://asciinema.org/a/617530)
+
+## Getting started
+
+### Pre-requisites
 - [Docker](https://docs.docker.com/engine/install/)
 - [kind](https://kind.sigs.k8s.io/)
 - [kubectl](https://kubernetes.io/docs/reference/kubectl/)
 - [Helm](https://helm.sh/docs/intro/install/)
+- [Bat](https://github.com/sharkdp/bat)
 
-## Usage
+### Usage
 `./demo.sh` to run the demo
 
 ## Architecture
--- demo.sh
+++ demo.sh
@@ -3,7 +3,9 @@
 . third_party/demo-magic/demo-magic.sh
 
 clear
+DEMO_PROMPT="${GREEN}➜  ${COLOR_RESET}"
 
+# setup begins
 echo "setting up kind cluster"
 pei "kind create cluster --name gk-demo --wait 5m"
 pei "kubectl version"
@@ -19,31 +21,64 @@ pei "helm upgrade --install redis bitnami/redis --namespace default --set image.
 echo "setting up subscriber"
 pei "kubectl create ns fake-subscriber"
 pei "kubectl get secret redis --namespace=default -o yaml | sed 's/namespace: .*/namespace: fake-subscriber/' | kubectl apply -f -"
-pei "kubectl apply -f dapr-component-subscriber.yaml"
+pei "kubectl apply -f manifests/dapr-component-subscriber.yaml"
 pei "docker build "https://github.com/open-policy-agent/gatekeeper.git#:test/pubsub/fake-subscriber" -t fake-subscriber:latest"
 pei "kind load docker-image --name gk-demo fake-subscriber:latest"
-pei "kubectl apply -f subscriber-deployment.yaml"
+pei "kubectl apply -f manifests/subscriber-deployment.yaml"
 pei "kubectl rollout status deployment -n fake-subscriber sub"
 
 echo "setting up Gatekeeper"
 pei "kubectl create namespace gatekeeper-system"
 pei "kubectl get secret redis --namespace=default -o yaml | sed 's/namespace: .*/namespace: gatekeeper-system/' | kubectl apply -f -"
-pei "kubectl apply -f dapr-component-gatekeeper.yaml"
-pei "kubectl apply -f gatekeeper-3.14-cel.yaml"
+pei "kubectl apply -f manifests/dapr-component-gatekeeper.yaml"
+pei "kubectl apply -f manifests/gatekeeper-3.14-cel.yaml"
 pei "kubectl rollout status deployment -n gatekeeper-system gatekeeper-controller-manager"
 pei "kubectl rollout status deployment -n gatekeeper-system gatekeeper-audit"
-pei "kubectl apply -f gatekeeper-configmap-pubsub.yaml"
+pei "kubectl apply -f manifests/gatekeeper-configmap-pubsub.yaml"
+# setup is complete
 
-echo "deploying cel and rego policies"
-pei "kubectl apply -f policies/constrainttemplate-cel.yaml"
-pei "kubectl apply -f policies/constraint-cel.yaml"
+# demo starts here
+echo "Let's get started by looking into components deployed into the cluster"
+pe "kubectl get pods --all-namespaces"
+wait
 
-pei "kubectl apply -f policies/constrainttemplate-rego.yaml"
-pei "kubectl apply -f policies/constraint-rego.yaml"
+clear
 
-kubectl logs -l app=sub -c go-sub -n fake-subscriber -f
+echo "First, we are going to deploy a Common Expression Language (CEL) based policy"
+wait
+bat policies/constrainttemplate-cel.yaml --paging never
+wait
+pe "kubectl apply -f policies/constrainttemplate-cel.yaml"
+bat policies/constraint-cel.yaml --paging never
+wait
+pe "kubectl apply -f policies/constraint-cel.yaml"
 
-# echo "cleanup cluster"
-# p "kind delete cluster --name gk-demo"
+clear
+
+echo "Next, we are going to deploy a Rego-based policy side-by-side with the CEL-based policy"
+wait
+bat policies/constrainttemplate-rego.yaml --paging never
+wait
+pe "kubectl apply -f policies/constrainttemplate-rego.yaml"
+bat policies/constraint-rego.yaml --paging never
+wait
+pe "kubectl apply -f policies/constraint-rego.yaml"
+
+clear
+
+echo "Let's look into violations coming from the CEL-based policy in constraint resource"
+wait
+pe "kubectl get k8srequiredlabels.constraints.gatekeeper.sh all-must-have-owner -o yaml"
+wait
+
+clear
+
+echo "Finally, these violations can be consumed by a subscriber via pubsub"
+wait
+pe "kubectl logs -l app=sub -c go-sub -n fake-subscriber -f"
 
 echo "More information can be found at https://open-policy-agent.github.io/gatekeeper/website/"
+
+# echo "cleanup"
+# kubectl delete constrainttemplate --all
+# kind delete cluster --name gk-demo
